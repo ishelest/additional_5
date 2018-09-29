b0617244e277e6d3c075100ca22038afdfdd07fb
Ivan Shelest <ishelest2012@gmail.com>
2018-09-29 22:40:19 +0300

    additional_5/feature: partial function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+//const bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
+//const str = '([{}])';
+//check('()', [['(', ')']]);
+//console.log(check('|()|', [['(', ')'], ['|', '|']]))
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  
+  let openStack = [];
+  let pairIndex;
+  
+  for (let i = 0, strLen = str.length; i < strLen; i++) {
+    
+    if (findConfig(i) && bracketsConfig[pairIndex][0] === str[i]) {
+      let pairIdentic = bracketsConfig[pairIndex][0] === bracketsConfig[pairIndex][1];
+      if (pairIdentic && openStack.length) {openStack.pop();
+        } else openStack.push(str[i]);
+      
+    } else if (findConfig(i) && bracketsConfig[pairIndex][1] === str[i]) {
+        if (openStack.length === 0) return false;
+        
+        else if (openStack[openStack.length - 1] === bracketsConfig[pairIndex][0]) {
+          openStack.pop();
+        }
+       } else return false;
+  }
+  return openStack.length == 0;
+  
+  
+  //find bracket pair in config and change pairIndex
+  function findConfig(i) {
+    for (let j = 0, cLen = bracketsConfig.length; j <= cLen; j++) {
+        //if (i > str.length || !i) break;
+        let k = bracketsConfig[j].indexOf(str[i]); 
+        if (k >= 0 && str[i] === bracketsConfig[j][k]) {
+            pairIndex = j;
+            return true;
+        } 
+    }
+
+  };
+
+};
