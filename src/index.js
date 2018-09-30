eac0904f920b381f5ed3beafcbe9c4e4cd3bdf1c
Ivan Shelest <ishelest2012@gmail.com>
2018-09-30 21:01:00 +0300

    additional_5/fix: Implemented support of identic opening and closing brackets

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-//const bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
-//const str = '([{}])';
-//check('()', [['(', ')']]);
-//console.log(check('|()|', [['(', ')'], ['|', '|']]))
 module.exports = function check(str, bracketsConfig) {
   
   let openStack = [];
   let pairIndex;
   
   for (let i = 0, strLen = str.length; i < strLen; i++) {
-    
     if (findConfig(i) && bracketsConfig[pairIndex][0] === str[i]) {
+      //check for closing identic bracket case
       let pairIdentic = bracketsConfig[pairIndex][0] === bracketsConfig[pairIndex][1];
-      if (pairIdentic && openStack.length) {openStack.pop();
+      let pairIdenticClosed = openStack[openStack.length - 1] === bracketsConfig[pairIndex][0];
+      if (pairIdentic && openStack.length && pairIdenticClosed) {openStack.pop();
         } else openStack.push(str[i]);
       
     } else if (findConfig(i) && bracketsConfig[pairIndex][1] === str[i]) {
@@ -23,8 +20,7 @@ module.exports = function check(str, bracketsConfig) {
        } else return false;
   }
   return openStack.length == 0;
-  
-  
+    
   //find bracket pair in config and change pairIndex
   function findConfig(i) {
     for (let j = 0, cLen = bracketsConfig.length; j <= cLen; j++) {
